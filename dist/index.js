ad2b58370687a37e0e50271b8dfa45acb04bff0f
BhairabChoudhury <bhairabchoudhury02@gmail.com>
2025-12-31 18:07:59 +0530

    Change Index

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_1 = require("../generated/prisma");
-const prisma = new prisma_1.PrismaClient();
+const client_1 = require("@prisma/client");
+const client = new client_1.PrismaClient();
+async function createUser() {
+    const user = await client.user.create({
+        data: {
+            name: "Bhairab",
+            email: "bhairab@example.com",
+            city: "Kolkata",
+        },
+    });
+    console.log("User Created:", user);
+}
+createUser()
+    .then(() => console.log("Done"))
+    .catch(err => console.error(err));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
+ const client = new PrismaClient() ;
+async function createUser() {
+  const user = await client.user.create({
+    data: {
+      name: "Bhairab",
+      email: "bhairab@example.com",
+      city: "Kolkata",
+    },
+  })
 
-const prisma = new PrismaClient();
+  console.log("User Created:", user)
+}
 
+createUser()
+  .then(() => console.log("Done"))
+  .catch(err => console.error(err))
